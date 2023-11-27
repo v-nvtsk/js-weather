ddfb91e90db3d748173ebe5c26620750e73478d1
v-nvtsk <v@nvtsk.ru>
2023-11-27 20:53:03 +0300

    update jest.config
    add tests pass threshold

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ const config = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -44,7 +44,14 @@ const config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
