5fa1086af3b849f951301e19077697c6b3a130e1
v-nvtsk <v@nvtsk.ru>
2023-12-03 01:52:43 +0300

    style: clear map.js from spaces

--- src/js/map.js
+++ src/js/map.js
@@ -2,18 +2,12 @@
 
 let Map;
 
-export async function initMap(coord) {
+export async function initMap(coordArr) {
   await global.ymaps3.ready;
-
   const { YMap, YMapDefaultSchemeLayer } = global.ymaps3;
-
   Map = new YMap(document.getElementById('map'), {
-    location: {
-      center: coord,
-      zoom: 10
-    }
+    location: { center: coordArr, zoom: 10 }
   });
-
   Map.addChild(new YMapDefaultSchemeLayer());
   return Map;
 }
@@ -22,11 +16,6 @@ export async function setLocation(coordArr) {
   if (!Map) {
     Map = await initMap(coordArr);
   } else {
-    Map.setLocation({
-      center: coordArr,
-      zoom: 10
-    });
+    Map.setLocation({ center: coordArr, zoom: 10 });
   }
 }
-
-// initMap();
