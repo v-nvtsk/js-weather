2a83fabb1f4a3c34310a62e30377e9d84847ae78
v-nvtsk <v@nvtsk.ru>
2023-12-02 14:20:07 +0300

    append index.js tests with input group

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,45 @@
-function appendParentWithEl(parentEl, tag, childId = '') {
+let currentWeatherEl;
+let currentCityEl;
+let currentTempEl;
+let currentIconEl;
+let inputCity;
+let submitBtn;
+
+function appendParentWithChild(parentEl, tag, childId = '') {
   const child = document.createElement(tag);
   child.id = childId;
   parentEl.append(child);
   return child;
 }
 
-export default function pageInit(parent) {
+export function pageInit(parent) {
 
-  const header = appendParentWithEl(parent, 'h1', 'header');
+  const header = appendParentWithChild(parent, 'h1', 'header');
   header.innerHTML = 'Weather forecast';
 
-  const currentWeatherEl = appendParentWithEl(parent, 'div', 'currentWeather');
-  const currentCityEl = appendParentWithEl(currentWeatherEl, 'div', 'currentCity');
-  const currentTempEl = appendParentWithEl(currentWeatherEl, 'div', 'currentTemp');
-  const currentIconEl = appendParentWithEl(currentWeatherEl, 'div', 'currentIcon');
+  currentWeatherEl = appendParentWithChild(parent, 'div', 'currentWeather');
+  currentCityEl = appendParentWithChild(currentWeatherEl, 'div', 'currentCity');
+  currentTempEl = appendParentWithChild(currentWeatherEl, 'div', 'currentTemp');
+  currentIconEl = appendParentWithChild(currentWeatherEl, 'div', 'currentIcon');
+
+  const inputGroup = appendParentWithChild(parent, 'div', 'inputGroup');
+  inputCity = appendParentWithChild(inputGroup, 'input', 'inputCity');
+  inputCity.placeholder = 'Enter city';
+  submitBtn = appendParentWithChild(inputGroup, 'button', 'submitBtn');
+  submitBtn.innerHTML = 'Get weather forecast';
+}
+
+export function weatherRender(weather) {
+  if (weather) {
+    currentCityEl.innerHTML = `Current city: ${weather.city}`;
+    currentTempEl.innerHTML = `Current temperature: ${weather.temp}`;
+    currentIconEl.innerHTML = `<img src=${weather.icon}>`;
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const appContainer = document.querySelector('#app');
   pageInit(appContainer);
+  weatherRender()
 })
 
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,12 +1,16 @@
-import pageInit from '../js/index';
+/* eslint-disable no-useless-escape */
+import { pageInit, weatherRender } from '../js/index';
 
 describe('Weather forecast app', () => {
   let currentWeatherEl;
   let currentCityEl;
   let currentTempEl;
   let currentIconEl;
+  let inputGroup;
+  let inputCity;
+  let submitBtn;
 
-  beforeEach(() => {
+  beforeAll(() => {
     const container = document.createElement('div')
     container.id = 'app';
     document.body.append(container)
@@ -17,15 +21,49 @@ describe('Weather forecast app', () => {
     currentCityEl = currentWeatherEl.querySelector('#currentCity');
     currentTempEl = currentWeatherEl.querySelector('#currentTemp');
     currentIconEl = currentWeatherEl.querySelector('#currentIcon');
-  });
-  it('pageInit is a function', () => {
-    expect(pageInit).toBeInstanceOf(Function);
-  })
-  it('pageInit creates initial markup', () => {
-    expect(currentWeatherEl).not.toBeNull();
-    expect(currentCityEl).not.toBeNull();
-    expect(currentTempEl).not.toBeNull();
-    expect(currentIconEl).not.toBeNull();
 
+    inputGroup = parent.querySelector('#inputGroup');
+    inputCity = inputGroup.querySelector('#inputCity');
+    submitBtn = inputGroup.querySelector('#submitBtn');
   });
+  describe('pageInit', () => {
+
+    it('pageInit is a function', () => {
+      expect(pageInit).toBeInstanceOf(Function);
+    })
+    it('pageInit creates initial markup', () => {
+      expect(currentWeatherEl).not.toBeNull();
+      expect(currentCityEl).not.toBeNull();
+      expect(currentTempEl).not.toBeNull();
+      expect(currentIconEl).not.toBeNull();
+    });
+
+    it('inputGroup is created', () => {
+      expect(inputGroup).not.toBeNull();
+      expect(inputCity).not.toBeNull();
+      expect(submitBtn).not.toBeNull();
+    })
+  })
+
+  describe('weatherRender', () => {
+    const weatherData = {
+      city: 'London',
+      temp: 10,
+      icon: 'https://openweathermap.org/img/wn/10d@2x.png'
+    }
+    beforeEach(() => {
+      weatherRender(weatherData);
+    })
+
+    it('weatherRender is a function', () => {
+      expect(weatherRender).toBeInstanceOf(Function);
+    })
+
+
+    it('weatherRender renders weather data', () => {
+      expect(currentCityEl.innerHTML).toEqual(`Current city: ${weatherData.city}`);
+      expect(currentTempEl.innerHTML).toEqual(`Current temperature: ${weatherData.temp}`);
+      expect(currentIconEl.innerHTML).toEqual(`<img src=\"${weatherData.icon}\">`);
+    })
+  })
 });
